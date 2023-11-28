8c60e98175c1343af77864606e5b731973219074
Matt McCollum <mmccollum31@gatech.edu>
2023-11-28 11:02:55 -0500

    Add temporary card faces

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import BackgroundImg from './assets/bg.png'
 import AppleImg from './assets/apple.png'
+import CyanCardBack from './assets/CyanCardBack.png';
+import CyanCardFront from './assets/CyanCardFront.png';
+import MagentaCardBack from './assets/MagentaCardBack.png';
+import MagentaCardFront from './assets/MagentaCardFront.png';
 
 import '../style.css'
 import Phaser from 'phaser'
@@ -18,11 +22,13 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('background', BackgroundImg);
         this.load.image('apple', AppleImg);
+        this.load.image('cyanCardBack', CyanCardBack);
     }
 
     create() {
         this.add.image(0, 0, 'background').setOrigin(0, 0);
         this.add.image(500, 350, 'apple').setOrigin(0, 0);
+        this.add.image(0, 0, 'cyanCardBack').setOrigin(0, 0).setScale(0.25);
     }
 
     update() {
