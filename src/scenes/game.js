252835788ba8839daa70f4cc41ad17e40771fc41
Matt McCollum <mmccollum31@gatech.edu>
2023-11-28 11:49:25 -0500

    Add card and text examples

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -5,6 +5,8 @@ import CyanCardFront from '../assets/CyanCardFront.png';
 import MagentaCardBack from '../assets/MagentaCardBack.png';
 import MagentaCardFront from '../assets/MagentaCardFront.png';
 
+
+
 const sizes = {
     width: 1000,
     height: 700
@@ -23,10 +25,46 @@ export default class Game extends Phaser.Scene {
     }
 
     create() {
+        let self = this;
         this.add.image(0, 0, 'background').setOrigin(0, 0);
         this.add.image(500, 350, 'apple').setOrigin(0, 0);
-        this.add.image(0, 0, 'cyanCardBack').setOrigin(0, 0).setScale(0.25);
-        this.add.image(150, 0, 'cyanCardFront').setOrigin(0, 0).setScale(0.25);
+
+        // text emample:
+        // creates text
+        this.startText = this.add.text(75, 350, ['START']).setFontSize(32).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive(); 
+        // color change while hovering over text
+        this.startText.on('pointerover', function (pointer) {
+            self.startText.setColor('#ff69b4');
+        });
+        // color change back to original
+        this.startText.on('pointerout', function (pointer) {
+            self.startText.setColor('#00ffff');
+        });
+
+        // card example:
+        // creates cards
+        this.newCard = this.add.image(0, 0, 'cyanCardBack').setOrigin(0, 0).setScale(0.25).setInteractive();
+        this.newerCard = this.add.image(150, 0, 'cyanCardFront').setOrigin(0, 0).setScale(0.25).setInteractive();
+        // makes cards a draggable object
+        this.input.setDraggable(this.newCard);
+        this.input.setDraggable(this.newerCard);
+        // handle drag events
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        });
+        // handle card pickup events
+        this.input.on('dragstart', function (pointer, gameObject) {
+            // tint on drag
+            gameObject.setTint(0xffff69);
+            // top on drag
+            self.children.bringToTop(gameObject);
+        });
+        // handle card drop events
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+            // tint back to normal on drop --- not working, added it to this.input.on('drop') instead
+            gameObject.setTint();
+        });
     }
 
     update() {
