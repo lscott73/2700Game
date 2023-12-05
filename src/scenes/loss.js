8c5ba83335107f8e9e7ae3c8a76d377d422a50fd
Lucy Scott <lscott73@gatech.edu>
2023-12-04 21:22:01 -0500

    start for win loss screen

---
+++ src/scenes/loss.js
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+const sizes = {
+    width: 1000,
+    height: 700
+}
+import BackgroundImg from '../assets/GameOverScreen.png';
+
+export default class loss extends Phaser.Scene {
+    constructor() {
+        super({ key: 'loss' });
+    }
+
+    preload() {
+        this.load.image('gameOverScreen', BackgroundImg);
+    }
+    create() {
+        this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'gameOverScreen');
+        const startOverPoint = {
+            x: this.game.renderer.width / 2,
+            y: this.game.renderer.height * 0.55
+        };
+        const startRadius = 200;
+        let startCircle = this.add.circle(startPoint.x, startPoint.y, startRadius, 0x0000ff, 0);
+        startCircle.setInteractive();
+        startCircle.on('pointerdown', () => {
+            this.scene.start('loadscreen')
+        })
+
+
+    }
+
+}
