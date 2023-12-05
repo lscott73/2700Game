b1109d37fcb460ed51371985371f5e6f7e4135e8
Matt McCollum <mmccollum31@gatech.edu>
2023-12-04 21:57:56 -0500

    asset loading and tying to images

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -4,6 +4,52 @@ import ExampleCardFront from '../assets/ExampleCardFront.png';
 import MagentaCardBack from '../assets/MagentaCardBack.png';
 import MagentaCardFront from '../assets/MagentaCardFront.png';
 
+// employee cards
+import EMP1 from '../assets/EMP1.png';
+import EMP2 from '../assets/EMP2.png';
+import EMP002 from '../assets/EMP002.png';
+import EMP011 from '../assets/EMP011.png';
+import EMP020 from '../assets/EMP020.png';
+import EMP021 from '../assets/EMP021.png';
+import EMP030 from '../assets/EMP030.png';
+import EMP041 from '../assets/EMP041.png';
+import EMP042 from '../assets/EMP042.png';
+import EMP080 from '../assets/EMP080.png';
+import EMP092 from '../assets/EMP092.png';
+import EMP101 from '../assets/EMP101.png';
+import EMP105 from '../assets/EMP105.png';
+import EMP200 from '../assets/EMP200.png';
+import EMP202 from '../assets/EMP202.png';
+import EMP210 from '../assets/EMP210.png';
+import EMP300 from '../assets/EMP300.png';
+import EMP303 from '../assets/EMP303.png';
+import EMP306 from '../assets/EMP306.png';
+import EMP310 from '../assets/EMP310.png';
+import EMP430 from '../assets/EMP430.png';
+import EMP500 from '../assets/EMP500.png';
+import EMP606 from '../assets/EMP606.png';
+import EMP820 from '../assets/EMP820.png';
+import EMP920 from '../assets/EMP920.png';
+
+// lawsuit cards
+import LAWcivil from '../assets/LAWcivil.png';
+import LAWclass from '../assets/LAWclass.png';
+import LAWdivorce from '../assets/LAWdivorce.png';
+import LAWdrug from '../assets/LAWdrug.png';
+import LAWeeoc from '../assets/LAWeeoc.png';
+import LAWfraud from '../assets/LAWfraud.png';
+import LAWlibel from '../assets/LAWlibel.png';
+import LAWmedical from '../assets/LAWmedical.png';
+import LAWmerger from '../assets/LAWmerger.png';
+import LAWrico from '../assets/LAWrico.png';
+import LAWsherman from '../assets/LAWsherman.png';
+import LAWslander from '../assets/LAWslander.png';
+import LAWtort from '../assets/LAWtort.png';
+import LAWwrongful from '../assets/LAWwrongful.png';
+
+
+
+
 import Card from '../helpers/card.js';
 import CardData from '../helpers/carddata.js';
 import Zone from '../helpers/zone.js';
@@ -28,11 +74,55 @@ export default class Game extends Phaser.Scene {
         this.load.image('cxampleCardFront', ExampleCardFront);
         this.load.image('magentaCardBack', MagentaCardBack);
         this.load.image('magentaCardFront', MagentaCardFront);
+
+        // employee cards
+        this.load.image('EMP1', EMP1);
+        this.load.image('EMP2', EMP2);
+        this.load.image('EMP002', EMP002);
+        this.load.image('EMP011', EMP011);
+        this.load.image('EMP020', EMP020);
+        this.load.image('EMP021', EMP021);
+        this.load.image('EMP030', EMP030);
+        this.load.image('EMP041', EMP041);
+        this.load.image('EMP042', EMP042);
+        this.load.image('EMP080', EMP080);
+        this.load.image('EMP092', EMP092);
+        this.load.image('EMP101', EMP101);
+        this.load.image('EMP105', EMP105);
+        this.load.image('EMP200', EMP200);
+        this.load.image('EMP202', EMP202);
+        this.load.image('EMP210', EMP210);
+        this.load.image('EMP300', EMP300);
+        this.load.image('EMP303', EMP303);
+        this.load.image('EMP306', EMP306);
+        this.load.image('EMP310', EMP310);
+        this.load.image('EMP430', EMP430);
+        this.load.image('EMP500', EMP500);
+        this.load.image('EMP606', EMP606);
+        this.load.image('EMP820', EMP820);
+        this.load.image('EMP920', EMP920);
+        // lawsuit cards
+        this.load.image('LAWcivil', LAWcivil);
+        this.load.image('LAWclass', LAWclass);
+        this.load.image('LAWdivorce', LAWdivorce);
+        this.load.image('LAWdrug', LAWdrug);
+        this.load.image('LAWeeoc', LAWeeoc);
+        this.load.image('LAWfraud', LAWfraud);
+        this.load.image('LAWlibel', LAWlibel);
+        this.load.image('LAWmedical', LAWmedical);
+        this.load.image('LAWmerger', LAWmerger);
+        this.load.image('LAWrico', LAWrico);
+        this.load.image('LAWsherman', LAWsherman);
+        this.load.image('LAWslander', LAWslander);
+        this.load.image('LAWtort', LAWtort);
+        this.load.image('LAWwrongful', LAWwrongful);
+        
     }
 
     create() {
         let self = this;
         this.add.image(0, 0, 'background').setOrigin(0, 0);
+        
         //this.add.image(500, 350, 'apple');
 
         // center board zone, not really a zone
@@ -97,9 +187,9 @@ export default class Game extends Phaser.Scene {
         // add basic cards for player
         for (let i = 0; i < 8; i++) {
             if (i < 4) {
-                this.playerDeck.push(new CardData(0, 0, 1, 0, 0, 0, 'cxampleCardFront', 3)); // sprite would be 'internSprite' or something
+                this.playerDeck.push(new CardData(0, 0, 1, 0, 0, 0, 'EMP1', 3)); // sprite would be 'internSprite' or something
             } else {
-                this.playerDeck.push(new CardData(0, 0, 0, 1, 0, 0, 'cxampleCardFront', 3));
+                this.playerDeck.push(new CardData(0, 0, 0, 1, 0, 0, 'EMP2', 3));
             }
             console.log("player deck: add a card with (" + this.playerDeck[i].cashValue + ", " + this.playerDeck[i].leverageValue + ")");
         }
@@ -109,46 +199,44 @@ export default class Game extends Phaser.Scene {
         //add cards to center deck
         let j = 0;
         while (j < 2) { // add two of each type of card
-            this.centerDeck.push(new CardData(1, 0, 2, 0, 0, 0, 'magentaCardFront', 0)); // Businessfish1
-            this.centerDeck.push(new CardData(2, 0, 2, 1, 0, 0, 'magentaCardFront', 0)); // Businessfish2
-            this.centerDeck.push(new CardData(3, 0, 3, 0, 0, 0, 'magentaCardFront', 0)); // Businessfish3
-            this.centerDeck.push(new CardData(4, 0, 3, 1, 0, 0, 'magentaCardFront', 0)); // Businessfish4
-            this.centerDeck.push(new CardData(5, 0, 5, 0, 0, 0, 'magentaCardFront', 0)); // Businessfish5
-            this.centerDeck.push(new CardData(6, 0, 4, 3, 0, 0, 'magentaCardFront', 0)); // Businessfish6
-            this.centerDeck.push(new CardData(8, 0, 8, 2, 0, 0, 'magentaCardFront', 0)); // Businessfish7
-            this.centerDeck.push(new CardData(10, 0, 9, 3, 0, 0, 'magentaCardFront', 0)); // Businessfish8
-            this.centerDeck.push(new CardData(1, 0, 0, 1, 1, 0, 'magentaCardFront', 0)); // Lawfish1
-            this.centerDeck.push(new CardData(2, 0, 0, 2, 0, 0, 'magentaCardFront', 0)); // Lawfish2
-            this.centerDeck.push(new CardData(4, 0, 0, 2, 1, 0, 'magentaCardFront', 0)); // Lawfish3
-            this.centerDeck.push(new CardData(5, 0, 0, 3, 0, 0, 'magentaCardFront', 0)); // Lawfish4
-            this.centerDeck.push(new CardData(6, 0, 0, 4, 1, 0, 'magentaCardFront', 0)); // Lawfish5
-            this.centerDeck.push(new CardData(7, 0, 0, 4, 2, 0, 'magentaCardFront', 0)); // Lawfish6
-            this.centerDeck.push(new CardData(10, 0, 0, 8, 0, 0, 'magentaCardFront', 0)); // Lawfish7
-            this.centerDeck.push(new CardData(11, 0, 0, 9, 3, 0, 'magentaCardFront', 0)); // Lawfish8
-            this.centerDeck.push(new CardData(3, 0, 0, 0, 1, 0, 'magentaCardFront', 0)); // Polifishian1
-            this.centerDeck.push(new CardData(3, 0, 1, 0, 1, 0, 'magentaCardFront', 0)); // Polifishian2
-            this.centerDeck.push(new CardData(3, 0, 0, 0, 2, 0, 'magentaCardFront', 0)); // Polifishian3
-            this.centerDeck.push(new CardData(5, 0, 2, 0, 2, 0, 'magentaCardFront', 0)); // Polifishian4
-            this.centerDeck.push(new CardData(7, 0, 3, 0, 3, 0, 'magentaCardFront', 0)); // Polifishian5
-            this.centerDeck.push(new CardData(9, 0, 1, 0, 5, 0, 'magentaCardFront', 0)); // Polifishian6
-            this.centerDeck.push(new CardData(11, 0, 3, 0, 6, 0, 'magentaCardFront', 0)); // Polifishian7
-            this.centerDeck.push(new CardData(13, 0, 6, 0, 6, 0, 'magentaCardFront', 0)); // Polifishian8
-            this.centerDeck.push(new CardData(0, 5, 0, 0, 5, 3, 'magentaCardFront', 0)); // Lawsuit - Libel
-
-            this.centerDeck.push(new CardData(0, 10, 0, 0, 10, 5, 'magentaCardFront', 0)); // Lawsuit - RICO
-
-            this.centerDeck.push(new CardData(0, 1, 0, 0, 1, 1, 'magentaCardFront', 0)); // Lawsuit - Merger
-            this.centerDeck.push(new CardData(0, 2, 0, 0, 2, 1, 'magentaCardFront', 0)); // Lawsuit - Slander
-            this.centerDeck.push(new CardData(0, 3, 0, 0, 3, 2, 'magentaCardFront', 0)); // Lawsuit - Wrongful Termination
-            this.centerDeck.push(new CardData(0, 6, 0, 0, 6, 3, 'magentaCardFront', 0)); // Lawsuit - Medical Malpractice
-            this.centerDeck.push(new CardData(0, 4, 0, 0, 4, 2, 'magentaCardFront', 0)); // Lawsuit - Tort
-            this.centerDeck.push(new CardData(0, 7, 0, 0, 7, 4, 'magentaCardFront', 0)); // Lawsuit - Drug Recall
-            this.centerDeck.push(new CardData(0, 2, 0, 0, 2, 1, 'magentaCardFront', 0)); // Lawsuit - Divorce
-            this.centerDeck.push(new CardData(0, 8, 0, 0, 8, 4, 'magentaCardFront', 0)); // Lawsuit - Class Action
-            this.centerDeck.push(new CardData(0, 1, 0, 0, 1, 1, 'magentaCardFront', 0)); // Lawsuit - Civil
-            this.centerDeck.push(new CardData(0, 9, 0, 0, 9, 5, 'magentaCardFront', 0)); // Lawsuit - EEOC
-            this.centerDeck.push(new CardData(0, 8, 0, 0, 8, 4, 'magentaCardFront', 0)); // Lawsuit - SEC Fraud
-            this.centerDeck.push(new CardData(0, 9, 0, 0, 9, 5, 'magentaCardFront', 0)); // Lawsuit - Sherman Act
+            this.centerDeck.push(new CardData(1, 0, 2, 0, 0, 0,   'EMP200', 0)); // Businessfish1
+            this.centerDeck.push(new CardData(2, 0, 2, 1, 0, 0,   'EMP210', 0)); // Businessfish2
+            this.centerDeck.push(new CardData(3, 0, 3, 0, 0, 0,   'EMP300', 0)); // Businessfish3
+            this.centerDeck.push(new CardData(4, 0, 3, 1, 0, 0,   'EMP310', 0)); // Businessfish4
+            this.centerDeck.push(new CardData(5, 0, 5, 0, 0, 0,   'EMP500', 0)); // Businessfish5
+            this.centerDeck.push(new CardData(6, 0, 4, 3, 0, 0,   'EMP430', 0)); // Businessfish6
+            this.centerDeck.push(new CardData(8, 0, 8, 2, 0, 0,   'EMP820', 0)); // Businessfish7
+            this.centerDeck.push(new CardData(10, 0, 9, 2, 0, 0,  'EMP920', 0)); // Businessfish8
+            this.centerDeck.push(new CardData(1, 0, 0, 1, 1, 0,   'EMP011', 0)); // Lawfish1
+            this.centerDeck.push(new CardData(2, 0, 0, 2, 0, 0,   'EMP020', 0)); // Lawfish2
+            this.centerDeck.push(new CardData(4, 0, 0, 2, 1, 0,   'EMP021', 0)); // Lawfish3
+            this.centerDeck.push(new CardData(5, 0, 0, 3, 0, 0,   'EMP030', 0)); // Lawfish4
+            this.centerDeck.push(new CardData(6, 0, 0, 4, 1, 0,   'EMP041', 0)); // Lawfish5
+            this.centerDeck.push(new CardData(7, 0, 0, 4, 2, 0,   'EMP042', 0)); // Lawfish6
+            this.centerDeck.push(new CardData(10, 0, 0, 8, 0, 0,  'EMP080', 0)); // Lawfish7
+            this.centerDeck.push(new CardData(11, 0, 0, 9, 2, 0,  'EMP092', 0)); // Lawfish8
+            this.centerDeck.push(new CardData(3, 0, 0, 0, 1, 0,   'EMP001', 0)); // Polifishian1
+            this.centerDeck.push(new CardData(3, 0, 1, 0, 1, 0,   'EMP101', 0)); // Polifishian2
+            this.centerDeck.push(new CardData(3, 0, 0, 0, 2, 0,   'EMP002', 0)); // Polifishian3
+            this.centerDeck.push(new CardData(5, 0, 2, 0, 2, 0,   'EMP202', 0)); // Polifishian4
+            this.centerDeck.push(new CardData(7, 0, 3, 0, 3, 0,   'EMP303', 0)); // Polifishian5
+            this.centerDeck.push(new CardData(9, 0, 1, 0, 5, 0,   'EMP105', 0)); // Polifishian6
+            this.centerDeck.push(new CardData(11, 0, 3, 0, 6, 0,  'EMP306', 0)); // Polifishian7
+            this.centerDeck.push(new CardData(13, 0, 6, 0, 6, 0,  'EMP606', 0)); // Polifishian8
+            this.centerDeck.push(new CardData(0, 5, 0, 0, 5, 3,   'LAWlibel', 0)); // Lawsuit - Libel
+            this.centerDeck.push(new CardData(0, 10, 0, 0, 10, 5, 'LAWrico', 0)); // Lawsuit - RICO
+            this.centerDeck.push(new CardData(0, 1, 0, 0, 1, 1,   'LAWmerger', 0)); // Lawsuit - Merger
+            this.centerDeck.push(new CardData(0, 2, 0, 0, 2, 1,   'LAWslander', 0)); // Lawsuit - Slander
+            this.centerDeck.push(new CardData(0, 3, 0, 0, 3, 2,   'LAWwrongful', 0)); // Lawsuit - Wrongful Termination
+            this.centerDeck.push(new CardData(0, 6, 0, 0, 6, 3,   'LAWmedical', 0)); // Lawsuit - Medical Malpractice
+            this.centerDeck.push(new CardData(0, 4, 0, 0, 4, 2,   'LAWtort', 0)); // Lawsuit - Tort
+            this.centerDeck.push(new CardData(0, 7, 0, 0, 7, 4,   'LAWdrug', 0)); // Lawsuit - Drug Recall
+            this.centerDeck.push(new CardData(0, 2, 0, 0, 2, 1,   'LAWdivorce', 0)); // Lawsuit - Divorce
+            this.centerDeck.push(new CardData(0, 8, 0, 0, 8, 4,   'LAWclass', 0)); // Lawsuit - Class Action
+            this.centerDeck.push(new CardData(0, 1, 0, 0, 1, 1,   'LAWcivil', 0)); // Lawsuit - Civil
+            this.centerDeck.push(new CardData(0, 9, 0, 0, 9, 5,   'LAWeeoc', 0)); // Lawsuit - EEOC
+            this.centerDeck.push(new CardData(0, 8, 0, 0, 8, 4,   'LAWfraud', 0)); // Lawsuit - SEC Fraud
+            this.centerDeck.push(new CardData(0, 9, 0, 0, 9, 5,   'LAWsherman', 0)); // Lawsuit - Sherman Act
             j++;
         }
         // initial shuffle of center deck
